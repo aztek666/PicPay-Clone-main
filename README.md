82edd1f1e64b3f6d3c239644cea0672f0339ec0c
Mathh <89319059+aztek666@users.noreply.github.com>
2021-08-21 18:10:38 -0300

    v1

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">
+  PicPay Clone
+</h1>
+
+<p align="center">
+  <img src="./preview.png" alt="preview.png"/>
+</p>
---
+++ src/screens/Pay/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Wrapper, Label } from './styles';
+
+export default function Home() {
+    return (
+        <Wrapper>
+            <Label>In maintence</Label>
+        </Wrapper>
+    );
+}
